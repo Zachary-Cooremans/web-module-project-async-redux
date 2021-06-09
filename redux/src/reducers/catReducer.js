8626da0298cb414e5a81d4e64581d3896a46aa27
Zachary Cooremans <zcooremans@gmail.com>
2021-06-09 15:05:19 -0500

    push

---
+++ redux/src/reducers/catReducer.js
@@ -0,0 +1,43 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL} from '../actions/catAction'
+const initialState = {
+    cat: {
+        data: {
+            id: 0,
+            url: "",
+            webpurl: "",
+            x: 0,
+            y: 0,
+        },
+        // picture: {
+        // large: "",
+        // medium: "",
+        // thumbnail: ""
+        // }
+    },
+    isFetching: false,
+    error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case(FETCH_START):
+        return({
+            ...state,
+            isFetching: true
+        })
+        case(FETCH_SUCCESS):
+        return({
+            ...state,
+            cat: action.payload,
+            isFetching: false
+        })
+        case(FETCH_FAIL):
+            return({
+            ...state,
+            error: action.payload,
+            isFetching: false
+        })
+        default:
+        return state;
+    }
+};
